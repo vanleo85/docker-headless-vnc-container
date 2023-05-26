93b3605703ec3f2f280216521c37b93d60291d99
Leonid Vanin <vanl@automacon.ru>
2023-05-26 03:39:17 +0300

    +

--- src/onec/scripts/install-fonts.sh
+++ src/onec/scripts/install-fonts.sh
@@ -6,7 +6,7 @@ echo "Установка шрифтов Microsoft"
 cp /etc/apt/sources.list /etc/apt/sources.list.default
 
 echo "deb http://ftp.ru.debian.org/debian bullseye main" >> /etc/apt/sources.list \
-  && "deb http://ftp.ru.debian.org/debian bullseye contrib" >> /etc/apt/sources.list \
+  && echo "deb http://ftp.ru.debian.org/debian bullseye contrib" >> /etc/apt/sources.list \
   && apt update \
   && apt install ttf-mscore* -qqy \
   && fc-cache -fv
