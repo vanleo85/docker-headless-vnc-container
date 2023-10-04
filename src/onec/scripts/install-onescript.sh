7e7650285232243571e4ec10ef568f7a462e7dca
Leonid Vanin <vanl@automacon.ru>
2023-10-04 10:56:00 +0300

    +

--- src/onec/scripts/install-onescript.sh
+++ src/onec/scripts/install-onescript.sh
@@ -5,10 +5,10 @@ ONESCRIPT_VERSION=$1
 ONESCRIPT_PACKAGES=$2
 
 apt-get install -qqy \
-    nupkg-newtonsoft.json.6.0.8 \
     dirmngr  \
     ca-certificates \
-    gnupg
+    gnupg \
+    nupkg-newtonsoft.json.6.0.8
 
 gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/debian stable-buster main" | tee /etc/apt/sources.list.d/mono-official-stable.list
