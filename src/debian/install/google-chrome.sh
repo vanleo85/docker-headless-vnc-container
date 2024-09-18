298b52c20d81be81b850d5ffc53d5eaee828dcb2
Leonid Vanin <vanl@automacon.ru>
2024-09-18 18:30:40 +0300

    +

--- src/debian/install/google-chrome.sh
+++ src/debian/install/google-chrome.sh
@@ -3,12 +3,16 @@
 set -e
 
 echo "Install Google Chrome"
-apt update
-apt install software-properties-common apt-transport-https ca-certificates curl -y
-curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
-echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | tee /etc/apt/sources.list.d/google-chrome.list
-apt update
-apt install google-chrome-stable -y
+#apt update
+#apt install software-properties-common apt-transport-https ca-certificates curl -y
+#curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+#echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | tee /etc/apt/sources.list.d/google-chrome.list
+#apt update
+#apt install google-chrome-stable -y
 
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 #ln -sfn /usr/bin/chromium /usr/bin/chromium-browser
-apt-get clean -y
+#apt-get clean -y
