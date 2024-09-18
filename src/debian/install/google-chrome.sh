8f101a538c94682a00a4ea74fe384038b4bacad2
Leonid Vanin <vanl@automacon.ru>
2024-09-18 19:39:45 +0300

    +

--- src/debian/install/google-chrome.sh
+++ src/debian/install/google-chrome.sh
@@ -6,9 +6,9 @@ echo "Install Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb -y
 
-last_row=$(tail -n 1 /usr/bin/google-chrome)
-head -n -1 /usr/bin/google-chrome > google-chrome-temp
-mv google-chrome-temp /usr/bin/google-chrome
-echo "$last_row --no-sandbox" >> /usr/bin/google-chrome
-chmod 777 /usr/bin/google-chrome
-rm google-chrome-stable_current_amd64.deb
+#last_row=$(tail -n 1 /usr/bin/google-chrome)
+#head -n -1 /usr/bin/google-chrome > google-chrome-temp
+#mv google-chrome-temp /usr/bin/google-chrome
+#echo "$last_row --no-sandbox" >> /usr/bin/google-chrome
+#chmod 777 /usr/bin/google-chrome
+#rm google-chrome-stable_current_amd64.deb
