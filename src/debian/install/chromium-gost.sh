e76ab9a29db1b6ed90846459077bef5f7486c93d
Leonid Vanin <vanl@automacon.ru>
2024-10-25 14:23:49 +0300

    +

--- src/debian/install/chromium-gost.sh
+++ src/debian/install/chromium-gost.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install Chromium Gost"
 wget -O chromium-gost.deb https://github.com/deemru/Chromium-Gost/releases/download/130.0.6723.58/chromium-gost-130.0.6723.58-linux-amd64.deb
-dpkg -i chromium-gost.deb
+apt install -qqy ./chromium-gost.deb
 apt-get clean -y
 
 
