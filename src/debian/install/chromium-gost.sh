f04759fc78a974280be3c652d61ade1e56c86ae4
Leonid Vanin <vanl@automacon.ru>
2024-10-25 14:55:16 +0300

    +

--- src/debian/install/chromium-gost.sh
+++ src/debian/install/chromium-gost.sh
@@ -5,6 +5,13 @@ set -e
 echo "Install Chromium Gost"
 wget -O chromium-gost.deb https://github.com/deemru/Chromium-Gost/releases/download/130.0.6723.58/chromium-gost-130.0.6723.58-linux-amd64.deb
 apt install -qqy ./chromium-gost.deb
+
+last_row=$(tail -n 1 /opt/chromium-gost/chromium-gost)
+head -n -1 /opt/chromium-gost/chromium-gost > chromium-gost-temp
+mv chromium-gost-temp /opt/chromium-gost/chromium-gost
+echo "$last_row --no-sandbox --no-default-browser-check --no-first-run" >> /opt/chromium-gost/chromium-gost
+chmod +x /opt/chromium-gost/chromium-gost
+rm ./chromium-gost.deb
 apt-get clean -y
 
 
